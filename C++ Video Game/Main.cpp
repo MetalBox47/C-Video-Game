7b3feefb6c07a9e6b7bb49cd7c2214be82ab3acb
MetalBox47 <35822953+MetalBox47@users.noreply.github.com>
2023-08-11 00:41:21 -0700

    Created a Window using SDL2

--- C++ Video Game/Main.cpp
+++ C++ Video Game/Main.cpp
@@ -1,11 +1,28 @@
 // C++ Video Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Needs SDL2 v2.28.2 from https://github.com/libsdl-org/SDL/releases/tag/release-2.28.2
+// Specifically the "SDL2-devel-2.28.2-VC.zip" package
 
+#include "SDL.h"
 #include <iostream>
+#include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
     std::cout << "Hello World!\n";
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
